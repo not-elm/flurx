f6e6f20f8a8db2c5dcc432e9b5c83c717e379bda
not-elm <elmgameinfo@gmail.com>
2024-03-11 21:23:34 +0900

    FIX: ReactiveFutures

--- src/scheduler/future.rs
+++ src/scheduler/future.rs
@@ -1,6 +1,7 @@
 use std::future::Future;
 use std::pin::Pin;
 use std::task::{Context, Poll};
+
 use crate::scheduler::Reactor;
 
 pub(crate) struct ReactorsFuture<'a, 'b> {
@@ -15,17 +16,49 @@ impl<'a, 'b> Future for ReactorsFuture<'a, 'b>
     type Output = ();
 
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        if let Some(mut reactor) = self.reactors.pop() {
+        if self.reactors.is_empty() {
+            return Poll::Ready(());
+        }
+
+        while let Some(mut reactor) = self.reactors.pop() {
             if reactor.as_mut().poll(cx).is_pending() {
                 self.polled.push(reactor);
             }
-            cx.waker().wake_by_ref();
-            Poll::Pending
+        }
+        if self.polled.is_empty() {
+            Poll::Ready(())
         } else {
             while let Some(f) = self.polled.pop() {
                 self.reactors.push(f);
             }
-            Poll::Ready(())
+            Poll::Pending
         }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::prelude::once;
+    use crate::Scheduler;
+    use crate::tests::result_event;
+
+    #[tokio::test]
+    async fn run_all_reactors() {
+        let mut scheduler = Scheduler::new();
+        let (tx, rx) = result_event::<usize>();
+        let tx2 = tx.clone();
+        scheduler.schedule(|tc| async move {
+            tc.task(once::run(|_| {
+                tx.set(tx.get() + 1);
+            })).await;
+        });
+        scheduler.schedule(|tc| async move {
+            tc.task(once::run(|_| {
+                tx2.set(tx2.get() + 1);
+            })).await;
+        });
+        scheduler.run(0).await;
+        assert_eq!(rx.get(), 2);
     }
 }
