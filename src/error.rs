231f43e025f397ddf4383457d30f1d48d79f7804
not-elm <elmgameinfo@gmail.com>
2024-04-25 22:12:32 +0900

    REMOVE: thiserror

--- src/error.rs
+++ src/error.rs
@@ -1,8 +1,15 @@
-use thiserror::Error;
+use std::fmt::{Display, Formatter};
 
 pub type FutureResult<Out> = Result<Out, AlreadyDroppedScheduler>;
 
 #[non_exhaustive]
-#[derive(Error, Debug, Eq, PartialEq, Copy, Clone, Hash, Ord, PartialOrd)]
-#[error("already dropped scheduler")]
+#[derive(Debug, Eq, PartialEq, Copy, Clone, Hash, Ord, PartialOrd)]
 pub struct AlreadyDroppedScheduler;
+
+impl Display for AlreadyDroppedScheduler {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_str("already dropped scheduler")
+    }
+}
+
+impl std::error::Error for AlreadyDroppedScheduler {}
